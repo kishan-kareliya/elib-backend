46bb4f9da02a68cc4acc897e7851b1f7571ec7f0
kishan-kareliya <kareliyakishan007@gmail.com>
2024-07-15 18:07:22 +0530

    add user registration endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,30 @@
 import { NextFunction, Request,Response } from "express";
 import userModel from "../models/userModel";
 import createHttpError from "http-errors";
+import bcrypt from 'bcrypt';
 
 const createUser = async (req: Request,res: Response,next: NextFunction) => {
+    const {name,email,password} = req.body;
+
     //check if email exist in db or not
-    const user = await userModel.findOne({email: req.body.email});
+    const user = await userModel.findOne({email: email});
     if(user){
         const error = createHttpError(409,"User Already Exist with this Email");
         return next(error);
     }
 
-    res.send("Register request get successfully");
+    //hash user password
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const newUser = await userModel.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    }) 
+
+    res.json({
+        id: newUser._id
+    });
 }
 
 export default {
