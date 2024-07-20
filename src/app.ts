d3168fe87017f0341ffd90e5e6427a60fc826887
kishan-kareliya <kareliyakishan007@gmail.com>
2024-07-20 12:44:48 +0530

    add cors and .env.example

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./routes/userRouter";
 import bookRouter from "./routes/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,15 @@
-import {config as conf} from 'dotenv'
+import { config as conf } from "dotenv";
 conf();
 
 const _config = {
-    port: process.env.PORT,
-    databaseUrl: process.env.MONGO_CONNECTION_STRING,
-    env: process.env.NODE_ENV,
-    jwtSecret: process.env.JWT_SECRET,
-    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    cloudinaryCloudApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudinaryCloudApiSecret: process.env.CLOUDINARY_API_SECRET
-}
+  port: process.env.PORT,
+  databaseUrl: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryCloudApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryCloudApiSecret: process.env.CLOUDINARY_API_SECRET,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
+};
 
 export const config = Object.freeze(_config);
